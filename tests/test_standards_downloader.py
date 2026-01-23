b43494f08d17d8d5e473231c6375290bbc4e6161
Laia Cabot <laiacabot42@gmail.com>
2026-01-23 19:40:53 +0100

    feat: Remove unused download methods from StandardsDownloader and add unit tests for MITRE Shield and Engage downloads

---
+++ tests/test_standards_downloader.py
@@ -0,0 +1,18 @@
+import pytest
+from unittest.mock import patch
+from data_raw.src.downloader import StandardsDownloader
+
+class TestStandardsDownloader:
+    @patch('data_raw.src.downloader.StandardsDownloader.download_file')
+    def test_download_mitre_shield(self, mock_download_file):
+        downloader = StandardsDownloader()
+        downloader.download_mitre_shield()
+        assert mock_download_file.called
+
+    @patch('data_raw.src.downloader.StandardsDownloader.download_file')
+    def test_download_mitre_engage(self, mock_download_file):
+        downloader = StandardsDownloader()
+        downloader.download_mitre_engage()
+        assert mock_download_file.called
+
+    # Add more tests for other methods as needed
